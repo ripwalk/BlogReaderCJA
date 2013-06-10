b9f0651a42c03c080604a8a84f8416449ec2eaa1
Caleb Abraham <cabraham@macintosh-4.home>
2013-06-10 18:54:25 -0400

    Initial Commit

---
+++ BlogReaderCJA/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlogReaderCJA
+//
+//  Created by Caleb Abraham on 6/10/13.
+//  Copyright (c) 2013 Caleb Abraham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlogReaderCJA/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlogReaderCJA
+//
+//  Created by Caleb Abraham on 6/10/13.
+//  Copyright (c) 2013 Caleb Abraham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
